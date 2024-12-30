3e492989d22af5e2aa05366225be27e2e5071acc
Atsushi0803 <atsushi.7624@gmail.com>
2024-12-30 22:01:03 +0900

    HOMEキーの挙動を変更する

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,18 @@
+local function smart_home()
+  -- カーソルがある行を文字列で取得
+  local line = vim.api.nvim_get_current_line()
+  -- 現在のカーソル位置を取得
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  -- 先頭にある空白と制御文字(タブ)にマッチさせる。戻り値はマッチした文字列または nil
+  local m = line:match('^[%s%c]+')
+  -- カーソルを移動させる(三項演算子で読みづらいけどluaでよく見るので)
+  vim.api.nvim_win_set_cursor(0, (m ~= nil and (col == 0 or m:len() < col)) and { row, m:len() } or { row, 0 })
+end
+
+vim.api.nvim_set_keymap('n', '<Home>', '', { callback = smart_home })
+vim.api.nvim_set_keymap('i', '<Home>', '', { callback = smart_home })
+vim.api.nvim_set_keymap('v', '<Home>', '', { callback = smart_home })
+
 vim.keymap.set("n", "gd", function()
   -- 現在のカーソル位置にある単語を取得
   local word = vim.fn.expand("<cword>")
@@ -11,3 +26,4 @@ vim.keymap.set("n", "gr", function()
   -- :Gtags -r コマンドを実行
   vim.cmd("Gtags -r " .. word)
 end, { noremap = true, silent = true })
+
